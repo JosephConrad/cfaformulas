16d2682c22ef967224dc3038a9949fad6d63712d
Konrad Lisiecki <knrd.lisiecki@gmail.com>
2015-05-23 23:38:06 +0200

    CFA: layout polishing and page info

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,8 +3,12 @@ function showAnswer() {
     var button = $("#showAnswerButton");
         //document.getElementById("showAnswerButton");
     var box = $("#answerBox");
-    box.show();
+    //box.show();
     button.hide();
+    box.css("visibility", "visible");
+    var box = $("#commentBox");
+    box.css("visibility", "visible");
+
         //document.getElementById("answerBox");
     //button.style.visibility = "collapse";
     //box.style.visibility = "visible";
