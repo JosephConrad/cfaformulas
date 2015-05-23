23a669441d98edd720884fe0ec5028f8a94839ec
Konrad Lisiecki <knrd.lisiecki@gmail.com>
2015-05-23 21:00:04 +0200

    CFA: mvp

---
+++ api/api.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+ 
+
+
+router.get('/formula', function(req, res) {
+    //var line = linesKeys[Math.floor(Math.random() * linesKeys.length)];
+    //var directions = lines[line];
+    //var station = Object.keys(directions)[0];
+    var question = {};
+
+    question["question"] = "Formula for correlation";
+    question["answer"]   = "$$r_{XY} = \frac{cov_{XY}}{(s_X)(s_Y)}$$";
+    res.send({"result": {"question": question["question"], "answer": question["answer"]}});
+});
+
+
+
+module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+var questions = {
+    //"ethics"       : {},
+    "Quantitative" : [
+        {"question" : "Formula for correlation",
+         "answer"   : "$$ r_{XY} = \\frac{cov_{XY}}{(s_X)(s_Y)} $$"
+        }
+    ],
+    "Economics"    : [
+        {"question" : "Taylor rule",
+         "answer"   : "$$\\text{central bank policy rate} = r_n + \\pi + \\alpha(\\pi - \\pi^*) + \\beta(y-y*) $$"
+        }
+    ],
+    "CorporateFin" : [
+        {"question" : "Calculation of residual inscome:",
+         "answer"   : "$$ RI_{t} = NI_{t} - r_e B_{t-1}  $$"
+        }
+    ]
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    var question = {};
+    var keys = Object.keys(questions);
+    var category = keys[Math.floor(Math.random() * keys.length)];
+    var qList = questions[category];
+    var question = qList[Math.floor(Math.random() * qList.length)];
+  res.render('index', { category: category, "question": question["question"], "answer": question["answer"]});
+});
+
+
+module.exports = router;
