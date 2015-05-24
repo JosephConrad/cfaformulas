98d02f9cc8549e4adea9547e401fa109ed29b4aa
Andrzej Skrodzki <endrjuskr@users.noreply.github.com>
2015-05-24 11:19:47 +0200

    Update main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,16 +1,16 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-63325889-1', 'auto');
+ga('send', 'pageview');
 
 function showAnswer() {
     var button = $("#showAnswerButton");
-        //document.getElementById("showAnswerButton");
     var box = $("#answerBox");
-    //box.show();
     button.hide();
     box.css("visibility", "visible");
     var box = $("#commentBox");
     box.css("visibility", "visible");
-
-        //document.getElementById("answerBox");
-    //button.style.visibility = "collapse";
-    //box.style.visibility = "visible";
-
 }
